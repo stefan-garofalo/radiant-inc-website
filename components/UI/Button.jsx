e7bf4baaa87bd68fc80300f9ad90fb953fb175ff
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2023-12-24 22:11:10 +0100

    feat(styleguide): hovers

--- components/UI/Button.jsx
+++ components/UI/Button.jsx
@@ -3,7 +3,7 @@ import IconArrowRight from '../icons/ArrowRight'
 export default function Button({ children, ...props }) {
   return (
     <button
-      className="inline-flex items-center gap-2 p-4 rounded-[4px] border border-dark-400 dark:border-dark-100 text-dark-400 dark:text-dark-100 title-xs"
+      className="inline-flex items-center gap-2 p-4 rounded-[4px] border border-dark-400 dark:border-dark-100 text-dark-400 dark:text-dark-100 title-xs hover:bg-dark-400 dark:hover:bg-dark-100 hover:text-dark-100 dark:hover:text-dark-400 transition-colors duration-300"
       {...props}
     >
       {children}
--- components/UI/Tag.jsx
+++ components/UI/Tag.jsx
@@ -1,7 +1,7 @@
 export default function Tag({ children, ...props }) {
   return (
     <button
-      className="px-3.5 py-1 rounded-[30px] border border-dark-400 dark:border-dark-100 text-dark-400 dark:text-dark-100 body-xl"
+      className="px-3.5 py-1 rounded-[30px] border border-dark-400 dark:border-dark-100 text-dark-400 dark:text-dark-100 body-xl hover:bg-dark-400 dark:hover:bg-dark-100 hover:text-dark-100 dark:hover:text-dark-400 transition-colors duration-300"
       {...props}
     >
       {children}
