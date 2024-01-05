90afb5db793a9390efa1666de2de40299e3f84c6
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-01-05 12:27:48 +0100

    style(toast): animation

--- components/Tag/ActiveTags.jsx
+++ components/Tag/ActiveTags.jsx
@@ -2,7 +2,7 @@
 
 import { useAtom } from 'jotai'
 import { tagAtom } from '@/store/atoms'
-import { AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import Toast from '@/components/UI/Toast'
 
@@ -14,17 +14,26 @@ export default function ActiveTags() {
       {tags.length > 0 && (
         <Toast persistent>
           <div className="flex items-start justify-between gap-10">
-            <div className="flex flex-wrap gap-1">
-              <span>Active tags: </span>
-              {tags.map((tag, i) => (
-                <span
-                  key={`active-tag-${i}`}
-                  className="font-bold after:content-[','] last:after:content-none"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={`list-${tags.length}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="flex flex-wrap gap-1"
+              >
+                <span>Active tags: </span>
+
+                {tags.map((tag, i) => (
+                  <span
+                    key={`active-${i}`}
+                    className="font-bold after:content-[','] last:after:content-none"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </motion.div>
+            </AnimatePresence>
             <button
               onClick={() => setTags([])}
               className="border rounded-3xl py-1.5 px-4 hover:bg-light-100 hover:border-light-100 hover:text-dark-100 transition-colors duration-300"
