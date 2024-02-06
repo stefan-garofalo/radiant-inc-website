d34cb138d486d755de08636908a300ef9a5e8643
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-02-06 02:16:14 +0100

    feat: page services

---
+++ app/(tagged)/services/page.jsx
@@ -0,0 +1,40 @@
+import prismic from '@/lib/prismic'
+import Tag from '@/components/UI/Tag'
+import SectionWorks from '@/components/Sections/Works'
+import { PrismicRichText } from '@prismicio/react'
+import SectionContact from '@/components/Sections/Contact'
+
+export default async function ServicesPage() {
+  const [services, works] = await Promise.all([
+    prismic.getCollection({ type: 'service' }),
+    prismic.getCollection({ type: 'work' }),
+  ])
+
+  return (
+    <main className="container">
+      <h1 className="py-16 title-lg">Services</h1>
+      {services.map((service, i) => (
+        <section key={`service-${i}`} className="grid grid-cols-2 py-8">
+          <h2 className="title-sm">{service.data.title}</h2>
+          <div className="flex flex-col gap-y-4">
+            <ul className="flex gap-x-2 gap-y-3 flex-wrap">
+              {service.tags.map((tag, j) => (
+                <li key={`service-${i}-tag-${j}`}>
+                  <Tag mode="light">{tag}</Tag>
+                </li>
+              ))}
+            </ul>
+            <PrismicRichText field={service.data.description} />
+          </div>
+          <SectionWorks
+            className="col-span-2"
+            content={works.filter(
+              (work) => work.data.service_type.uid === service.uid
+            )}
+          ></SectionWorks>
+        </section>
+      ))}
+      <SectionContact />
+    </main>
+  )
+}
--- components/Sections/Works.jsx
+++ components/Sections/Works.jsx
@@ -1,9 +1,9 @@
 import ArrowTitle from '../ArrowTitle'
 import FilteredList from '@/components/Tag/FilteredList'
 
-export default function SectionWorks({ content }) {
+export default function SectionWorks({ content, className = '' }) {
   return (
-    <section id="works" className="py-16 overflow-hidden">
+    <section id="works" className={`py-16 overflow-hidden ${className}`}>
       <ArrowTitle className="pl-container py-8">Work</ArrowTitle>
       <FilteredList items={content} />
     </section>
