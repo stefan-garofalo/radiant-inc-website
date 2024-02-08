2689aff3914f0ac6b1a87998124256975d5e476e
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-02-08 23:59:17 +0100

    style(section-contact): mobile

--- components/Sections/Contact.jsx
+++ components/Sections/Contact.jsx
@@ -1,11 +1,11 @@
 export default function SectionContact() {
   return (
     <section className="py-32 flex flex-col items-center justify-center gap-y-1">
-      <h3 className="title-lg">Contact us</h3>
+      <h3 className="title-md lg:title-lg">Contact us</h3>
       <a
         href="https://dictionary.cambridge.org/dictionary/english/inquiry"
         target="_blank"
-        className="body-lg hover:opacity-50 transition-opacity duration-300"
+        className="body-md lg:body-lg hover:opacity-50 transition-opacity duration-300"
       >
         Please send inquiries to Radiant Inc. From here
       </a>
