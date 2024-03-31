1e02838b138c54b8132ec92d964a423aca2fb1d2
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-03-31 03:09:58 +0200

    style(company): mobile

--- app/(untagged)/company/page.jsx
+++ app/(untagged)/company/page.jsx
@@ -20,21 +20,21 @@ export default async function CompanyPage() {
   const page = await prismic.getSingleton('company')
   return (
     <main className="container">
-      <section className="py-16 grid grid-cols-2">
-        <h1 className="title-sm" id="company">Company</h1>
-        <div className="flex flex-col gap-y-6 body-xl">
-          <h2 className="title-md">{page.data.title}</h2>
+      <section className="8 lg:py-16 grid lg:grid-cols-2 gap-y-5">
+        <h1 className="title-xs lg:title-sm" id="company">Company</h1>
+        <div className="flex flex-col gap-y-6 body-lg lg:body-xl">
+          <h2 className="title-sm lg:title-md">{page.data.title}</h2>
           <Picture className="aspect-cover rounded-3xl" image={page.data.cover}></Picture>
           <PrismicRichText field={page.data.paragraph}></PrismicRichText>
         </div>
       </section>
-      <section className="py-18 grid grid-cols-2 body-xl">
+      <section className="py-10 lg:py-18 grid lg:grid-cols-2 body-lg lg:body-xl">
         <h2 className="title-sm" id="history">History</h2>
-        <div className="flex flex-col gap-y-6">
+        <div className="pt-6 lg:pt-0 flex flex-col gap-y-6">
           <PrismicRichText field={page.data.milestones_paragraph}></PrismicRichText>
           <ol className="flex flex-col">
             {page.data.milestones.map((milestone, index) => (
-              <li key={`milestone-${index}`} className="py-8 border-b flex items-center gap-10 body-md">
+              <li key={`milestone-${index}`} className="py-6 lg:py-8 border-b flex items-center gap-5 lg:gap-10 body-md">
                 <span>{new Date(milestone.date).getFullYear()}</span>
                 <span>
                   {new Date(milestone.date).toLocaleString('en-US', {
